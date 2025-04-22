2a9bf44cfc6f8246387d9b887beec0bca1b930d7
nurulislamrimon <nurulislamrimon@gmail.com>
2025-04-22 15:19:00 +0600

    [CONTENT/UPDATED]: greeting message updated

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello() {
-    return { data: 'Hello Man! Thanks for being with Expertsquad 🌹' };
+    return { data: 'Hello Man! Thanks for being with Combination Of Power 🌹' };
   }
 }
