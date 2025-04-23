f147b0424a5fe284c715df61ab1507ef42a5c09f
nurulislamrimon <nurulislamrimon@gmail.com>
2025-04-23 11:38:35 +0600

    [VERCEL_CORS_RULE/ADDED]: dist file updated

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+// main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
@@ -7,13 +8,13 @@ import { ValidationPipe } from '@nestjs/common';
 import { ResponseInterceptor } from './interceptors/response.interceptor';
 import { seedService } from './v1/seed.service';
 
-async function bootstrap() {
+export async function createApp(): Promise<NestExpressApplication> {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   // seed
   void seedService();
 
-  // cors controle
+  // cors control
   const allowedOrigins = envConfig.client_url?.split(',') || [
     'http://localhost:3000',
   ];
@@ -23,24 +24,23 @@ async function bootstrap() {
     credentials: true,
   });
 
-  // static repository
+  // static repo
   app.useStaticAssets(join(__dirname, '..', 'public'));
 
-  // api prefix to the path
+  // api prefix
   app.setGlobalPrefix('api');
 
-  // validation pipe
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      transform: true,
-    }),
-  );
+  // pipes
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
-  // response format
+  // interceptors
   app.useGlobalInterceptors(new ResponseInterceptor());
 
-  // listen app
-  await app.listen(envConfig.port);
+  await app.init();
+  return app;
 }
-void bootstrap();
+
+// CLI use
+if (require.main === module) {
+  void createApp().then((app) => app.listen(envConfig.port));
+}
