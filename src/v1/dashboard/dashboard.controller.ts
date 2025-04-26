730c9744e089725ad1d2c59d20491aeedb675339
nurulislamrimon <nurulislamrimon@gmail.com>
2025-04-26 11:49:47 +0600

    [DASHBOARD_ROUTE/OPENED]

--- src/v1/dashboard/dashboard.controller.ts
+++ src/v1/dashboard/dashboard.controller.ts
@@ -1,18 +1,19 @@
 import { Controller, Get, Req } from '@nestjs/common';
 import { DashboardService } from './dashboard.service';
-import { Public } from 'src/decorators/public.decorator';
 import { Request } from 'express';
 import { JwtPayload } from 'jsonwebtoken';
+import { Public } from 'src/decorators/public.decorator';
 
 @Controller('v1/dashboard')
 export class DashboardController {
-  constructor(private readonly dashboardService: DashboardService) { }
+  constructor(private readonly dashboardService: DashboardService) {}
 
   /**
-  * API: Service
-  * Message: Get - statistics from snapshot and member
-  */
+   * API: Service
+   * Message: Get - statistics from snapshot and member
+   */
   @Get()
+  @Public()
   async getSnapshotData() {
     return await this.dashboardService.getTotalModelsCounted();
   }
@@ -24,7 +25,7 @@ export class DashboardController {
   @Get('/last-month-statistics')
   async lastMonthStatistics(@Req() req: Request) {
     const user = req.user as JwtPayload;
-    return await this.dashboardService.lastMonthStatistics(user.id);
+    return await this.dashboardService.lastMonthStatistics(user.id as number);
   }
 
   /**
@@ -34,6 +35,6 @@ export class DashboardController {
   @Get('/last-year-statistics')
   async lastYearStatistics(@Req() req: Request) {
     const user = req.user as JwtPayload;
-    return await this.dashboardService.lastYearStatistics(user.id);
+    return await this.dashboardService.lastYearStatistics(user.id as number);
   }
 }
