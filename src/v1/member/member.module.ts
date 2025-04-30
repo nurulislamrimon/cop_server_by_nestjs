1cff6c6a2c35237843c22b873680c64e7a1821bc
nurulislamrimon <nurulislamrimon@gmail.com>
2025-04-30 23:28:28 +0600

    MEMBER_ISSUE/RESOLVED

--- src/v1/member/member.module.ts
+++ src/v1/member/member.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { MemberService } from './member.service';
 import { MemberController } from './member.controller';
 import { MemberSessionService } from '../member-session/member-session.service';
+import { TransactionService } from '../transaction/transaction.service';
 
 @Module({
   controllers: [MemberController],
-  providers: [MemberService, MemberSessionService],
+  providers: [MemberService, TransactionService, MemberSessionService],
 })
-export class MemberModule {}
+export class MemberModule { }
